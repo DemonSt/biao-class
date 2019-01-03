58cd2a609a76442f675a3d3c844a8e4108fbdf2a
DemonSt <demonsalvat@gmail.com>
2019-01-03 15:06:29 +0800

    jianshu

---
+++ practice/131-react_jianshu/jianshu/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import Header from './common/header';
+import { GlobalStyle } from './style.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <Header />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
