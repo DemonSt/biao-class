d1ff69b4d43858aae0562a1aa2c26c0ff5304275
DemonSt <demonsalvat@gmail.com>
2019-01-18 22:50:11 +0800

    change

--- practice/116-vue-tarvel/src/pages/home/components/Header.vue
+++ practice/116-vue-tarvel/src/pages/home/components/Header.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-    export default{
+    export default {
         name: 'Header',
     }
 </script> 
---
+++ practice/116-vue-tarvel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeSwiper',
+        data () {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [
+                    {
+                        id: '001',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190109_dujia_homepage_top_banner_4.jpg'
+                    },
+                    {
+                        id: '002',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190115_dujia_homepage_2.jpg'
+                    },
+                    {
+                        id: '003',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190115_qunar_homepage_dujia_6.jpg'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff; 
+    .wrapper
+        overflow: hidden;
+        width: 100%;
+        height: 0;
+        padding-bottom: 31.25%;
+        .swiper-img
+            width: 100%;
+</style>
